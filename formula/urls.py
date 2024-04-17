0caef7167d4ae983d37414b0c9b77b835982acca
unknown <dionatanmaicon10@gmail.com>
2024-04-17 06:53:16 -0300

    ultimas alterações

--- formula/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('formula/', views.binomial_calc, name='binomial_calc'),
-]
--- formulaBi/urls.py
+++ formulaBi/urls.py
@@ -1,8 +1,8 @@
-from formula import urls
+from formula import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls))
+    path('', views.binomial_calc, name='binomial_calc'),
 ]
